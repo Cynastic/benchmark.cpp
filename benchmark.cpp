9c44260e8ebdfa0e5112fb405764925c9bc6b0b6
Cynastic <111385987+Cynastic@users.noreply.github.com>
2025-03-13 14:01:21 +0100

    initial functionality

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,1 +1,36 @@
-"" 
+#include "benchmark.h"
+#include <chrono>
+#include <ratio>
+
+void Benchmark::Start() {
+	startTime = high_resolution_clock::now();
+}
+
+void Benchmark::End() {
+	endTime = high_resolution_clock::now();
+	elapsedTime = (endTime - startTime);
+}
+
+long double Benchmark::GetNanoseconds() {
+	return duration<long double, nanoseconds::period>(elapsedTime).count();
+}
+
+long double Benchmark::GetMicroseconds() {
+	return duration<long double, microseconds::period>(elapsedTime).count();
+}
+
+long double Benchmark::GetMilliseconds() {
+	return duration<long double, milliseconds::period>(elapsedTime).count();
+}
+
+long double Benchmark::GetSeconds() {
+	return duration<long double, seconds::period>(elapsedTime).count();
+}
+
+long double Benchmark::GetMinutes() {
+	return duration<long double, minutes::period>(elapsedTime).count();
+}
+
+duration<long double> Benchmark::GetElapsedTimeSpan() {
+	return duration<long double>(elapsedTime);
+}
--- benchmark.h
+++ benchmark.h
@@ -1,1 +1,23 @@
-"" 
+#pragma once
+#include <chrono>
+
+using namespace std::chrono;
+
+class Benchmark {
+	public:
+		void Start();
+		void End();
+
+		long double GetNanoseconds();
+		long double GetMicroseconds();
+		long double GetMilliseconds();
+		long double GetSeconds();
+		long double GetMinutes();
+
+		duration<long double> GetElapsedTimeSpan();
+
+	private:
+		duration<long double> elapsedTime;
+		time_point<high_resolution_clock> startTime;
+		time_point<high_resolution_clock> endTime;
+};
